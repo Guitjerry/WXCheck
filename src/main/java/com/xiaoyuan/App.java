624af2e3168a918ff44b21cd5dd063994d8a79e8
jsd12060123@163.com <geli1990>
2017-10-16 15:00:18 +0800

    优化

---
+++ src/main/java/com/xiaoyuan/App.java
@@ -0,0 +1,18 @@
+package com.xiaoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+}
